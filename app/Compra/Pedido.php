ce500617e743bc8d7c5e6c8d6be67bcc96d4b89d
rebecastilho <becacastilhocamargo@gmail.com>
2019-03-26 10:47:40 -0300

    Criação de Models e Relações

---
+++ app/Compra/Pedido.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    protected $table = 'pedido';
+    public $timestamps = true;
+    protected $fillable = array('id','status');
+
+    //Relação com a tabela ListaCompra
+    public function itens(){
+        return $this->belongsToMany('App\Compra\ItemCompra','item_compra_has_pedido');
+
+    }
+
+    //Relação com a tabela Orcamento
+
+    public function orcamentos(){
+        return $this->belongsToMany('App\Compra\Orcamento','orcamento_has_pedido');
+    }
+
+}
