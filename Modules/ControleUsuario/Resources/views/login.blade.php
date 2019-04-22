d6a144f330c43624697ed0d4277d91c937da84ea
Giovanna Di Giaimo <gio.dg.silva@gmail.com>
2019-04-22 20:17:15 -0300

    Alterações tela de login

--- Modules/ControleUsuario/Resources/views/login.blade.php
+++ Modules/ControleUsuario/Resources/views/login.blade.php
@@ -4,6 +4,7 @@
 @section('content')
     <div class="row justify-content-center align-items-center" style="height:100%">
         <div class="col-xm-12 col-sm-10 col-md-8 col-lg-6">
+            <h3>Login</h3><br>
             <form>
                 <div class="form-group">
                     <label for="inputEmail">Endereço de email</label>
@@ -13,7 +14,7 @@
                     <label for="inputPassword">Senha</label>
                     <input type="password" class="form-control" id="inputPassword" placeholder="Senha">
                 </div>
-                <p>Esqueci minha senha</p>
+                <p><a href="">Esqueci minha senha</a></p>
                 <button type="submit" class="btn btn-primary">Entrar</button>
             </form>    
         </div>
