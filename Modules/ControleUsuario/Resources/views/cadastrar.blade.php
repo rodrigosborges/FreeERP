0fe661645b8be6a3bc6f09119815d20d0c032d79
adstonoliveira <suportetecnico.adston@gmail.com>
2019-04-22 11:44:22 -0300

    Criacao tela cadastro

--- Modules/ControleUsuario/Resources/views/cadastrar.blade.php
+++ Modules/ControleUsuario/Resources/views/cadastrar.blade.php
@@ -2,24 +2,33 @@
 @section('title', 'Cadastrar')
 
 @section('content')
-    <div class="row justify-content-center align-items-center" style="height:100%">
-        <div class="col-xm-12 col-sm-10 col-md-8 col-lg-6">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Endereço de email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
-                    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Senha</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>    
-        </div>
-    </div>
+<div class="d-flex p-2 bd-highlight col justify-content-sm-center border rounded">
+
+        <form action="#" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="nome">Nome de usu&aacute;rio</label>
+                <input type="text" class="form-control" id="nome" placeholder="Nome de usu&aacute;rio" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Endereço de email</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Seu email" required>
+                <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Senha</label>
+                <input type="password" class="form-control" id="password" placeholder="Senha" required>
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1" required>Clique em mim</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Definir permiss&otilde;es</button>
+        </form>
+
+</div>
+
 @endsection
