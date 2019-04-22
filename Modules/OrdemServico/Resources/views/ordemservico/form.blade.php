4cb4dea63add1d621965b1e3394fccace4b78125
rebecastilho <becacastilhocamargo@gmail.com>
2019-04-22 14:55:24 -0300

    arrumando formulario

--- Modules/OrdemServico/Resources/views/ordemservico/form.blade.php
+++ Modules/OrdemServico/Resources/views/ordemservico/form.blade.php
@@ -1,40 +1,48 @@
 @extends('template')
 @section('content')
-                
-    <form action="{{ $data['url'] }}" method="POST">
-        {{ csrf_field() }}
-        @if($data['model'])
+
+<div class="card " style="margin:auto; max-width: 40rem;">
+    <div class="card-header text-white bg-dark">Abrir OS</div>
+    <div class="card-body">
+        <form action="{{ $data['url'] }}" method="POST">
+            {{ csrf_field() }}
+            @if($data['model'])
             @method('PUT')
-        @endif
-      
-        <div class="form-group">
-        <div class="form-row">
-                <input type="text" placeholder="Solicitante" name="solicitante_id" id="solicitante_id" class="form-control" value="{{ $data['model'] ? $data['model']->solicitante_id : old('solicitante_id', "") }}">
-            </div>
-        </div>
-    
-        <div class="form-row">
-            <div class="col-md-4 mb-3">
-                <input type="text" placeholder="Tipo Aparelho" name="tipo_aparelho" id="tipo_aparelho" class="form-control" value="{{ $data['model'] ? $data['model']->tipo_aparelho : old('tipo_aparelho', "") }}">
+            @endif
+
+            <div class="form-group">
+                <div class="form-row">
+                    <label>Solicitante</label>
+                    <input type="text" placeholder="Solicitante" name="solicitante_id" id="solicitante_id" class="form-control" value="{{ $data['model'] ? $data['model']->solicitante_id : old('solicitante_id', "") }}">
+                </div>
             </div>
 
+            <div class="form-row">
+                <div class="col-md-4 mb-3">
+                    <label>Tipo do Aparelho</label>
+                    <input type="text" placeholder="ex: computador" name="tipo_aparelho" id="tipo_aparelho" class="form-control" value="{{ $data['model'] ? $data['model']->tipo_aparelho : old('tipo_aparelho', "") }}">
+                </div>
 
-        <div class="col-md-4 mb-3">
-            <input type="text" placeholder="Marca" name="marca" id="marca" class="form-control" value="{{ $data['model'] ? $data['model']->marca : old('marca', "") }}">
-        </div>
+                <div class="col-md-4 mb-3">
+                    <label>Marca</label>
+                    <input type="text" placeholder="Marca" name="marca" id="marca" class="form-control" value="{{ $data['model'] ? $data['model']->marca : old('marca', "") }}">
+                </div>
 
-    <div class="col-md-4 mb-3">
-            <input placeholder="Número de Série" type="text" name="numero_serie" id="numero_serie" class="form-control" value="{{ $data['model'] ? $data['model']->numero_serie : old('numero_serie', "") }}">
-        </div>
-        </div>
+                <div class="col-md-4 mb-3">
+                    <label>Número de Série</label>
+                    <input placeholder="Número de Série" type="text" name="numero_serie" id="numero_serie" class="form-control" value="{{ $data['model'] ? $data['model']->numero_serie : old('numero_serie', "") }}">
+                </div>
+            </div>
 
-        <div class="form-group">
-            <textarea placeholder='Descrição do Problema' name="descricao_problema" id="descricao_problema" class="form-control" >{{ $data['model'] ? $data['model']->descricao_problema : old('descricao_problema', "") }}</textarea>
-        </div>
-      
-        <div class="form-group">
-            <button type="submit" class="btn btn-success"> {{ $data['button'] }} </button> 
-            <a class="btn btn-light" href="{{ url('ordemservico/os') }}"><i class="glyphicon glyphicon-chevron-left"></i> Voltar</a>
-        </div>
-    </form>
+            <div class="form-group">
+                <textarea placeholder='Descrição do Problema' name="descricao_problema" id="descricao_problema" class="form-control">{{ $data['model'] ? $data['model']->descricao_problema : old('descricao_problema', "") }}</textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-success"> {{ $data['button'] }} </button>
+                <a href="{{ url('ordemservico/os') }}" class="btn btn-primary">Voltar</a>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
--- Modules/OrdemServico/Resources/views/ordemservico/index.blade.php
+++ Modules/OrdemServico/Resources/views/ordemservico/index.blade.php
@@ -1,49 +1,47 @@
 @extends('template')
 @section('content')
-   
-   <form method="GET" action="#">
+
+<form method="GET" action="#">
     <div class="form-row form-group">
         {!! Form::label('busca', 'Procurar por', ['class' => 'col-sm-2 col-form-label text-right']) !!}
-        <div class="col-sm-8">
+        <div class="input-group col-sm-8">
             {!! Form::text('busca', isset($busca) ? $busca : null, ['class' => 'form-control']) !!}
+          <div class="input-group-append">
+            {!! Form::submit('Buscar', ['class'=>'btn btn-primary']) !!}
         </div>
-
-        <div class="col-sm-2">
-            {!! Form::submit('procurar', ['class'=>'btn btn-primary']) !!}
-        </div>
+    </div>
     </div>
 </form>
 
-    <div class="text-center">
-            <table class="table table-striped table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Solicitante</th>
-                        <th>Descrição</th>
-                        <th ><a class="btn btn-success center-block" href="{{ url('ordemservico/os/create')  }}">Abrir OS</a></th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($data['ordem_servico'] as $os)
-                    <tr>
-                        <td>{{$os->solicitante_id}}</td>
-                        <td>{{$os->descricao_problema}}</td>
-                        <td>
-                            <form action="{{url('ordemservico/os',[$os->id])}}" method="POST">
-                                {{method_field('DELETE')}}
-                                {{ csrf_field() }}
-                                <a class="btn btn-outline-warning btn-sm" href='{{ url("ordemservico/os/$os->id/") }}'>Detalhes</a> 
-                                <a class="btn btn-outline-info btn-sm" href='{{ url("ordemservico/os/$os->id/edit") }}'>Editar</a> 
-                                <input type="submit" class="btn btn-outline-danger btn-sm" value="Delete"/>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+<div class="text-center">
+    <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>Solicitante</th>
+                <th>Descrição</th>
+                <th><a class="btn btn-success center-block" href="{{ url('ordemservico/os/create')  }}">Abrir OS</a></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data['ordem_servico'] as $os)
+            <tr>
+                <td>{{$os->solicitante_id}}</td>
+                <td>{{$os->descricao_problema}}</td>
+                <td>
+                    <form action="{{url('ordemservico/os',[$os->id])}}" method="POST">
+                        {{method_field('DELETE')}}
+                        {{ csrf_field() }}
+                        <a class="btn btn-outline-warning btn-sm" href='{{ url("ordemservico/os/$os->id/") }}'>Detalhes</a>
+                        <a class="btn btn-outline-info btn-sm" href='{{ url("ordemservico/os/$os->id/edit") }}'>Editar</a>
+                        <input type="submit" class="btn btn-outline-danger btn-sm" value="Delete" />
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
-        <div  class="row justify-content-center">
-
- {{ $data['ordem_servico']->links() }}
- </div>
+<div class="row justify-content-center">
+    {{ $data['ordem_servico']->links() }}
+</div>
 @endsection
