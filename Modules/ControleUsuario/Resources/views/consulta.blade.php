f027d364eb6d26e357a7abe7bee740a83bce6c23
Kevin Natan <kevinnatan97@gmail.com>
2019-04-29 19:41:50 -0300

    alteraçaõ na tela de busca

--- Modules/ControleUsuario/Resources/views/consulta.blade.php
+++ Modules/ControleUsuario/Resources/views/consulta.blade.php
@@ -1,25 +1,17 @@
 @extends('template')
 @section('title', 'Cadastrar')
 @section('content')
-   
-<div class="row justify-content-center">
-    <div class="col col-sm-10 col-md-8 col-lg-6">	
-       <div class="form-group">
-          <input type="email" class="form-control" placeholder="Buscar">
-       </div>
-    </div>
-       <button type="submit" class="btn btn-primary d-flex align-items-center">
-          <i class="material-icons mr-2">search</i>Buscar
-       </button>
 
-       <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">OPZAO</button>
-          <div class="dropdown-menu">
-             <a class="dropdown-item" href="#">opcao0</a>
-             <a class="dropdown-item" href="#">opcao1</a>
-             <a class="dropdown-item" href="#">opcao 2</a>
-    </div>
-    
+<div class="d-flex justify-content-center">
+<div class="form-group">
+         <input type="email" class="form-control" placeholder="Buscar">
+      </div>
+      <button type="button" class="btn btn-primary">
+         <i class="material-icons mr-2">search</i>Buscar
+      </button>    
 </div>
+
+   
 
 
 @endsection
