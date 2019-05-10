c3923488129e35ebf7da0581ffd5d9f82cbe9611
adston oliveira <suportetecnico.adston@gmail.com>
2019-05-10 13:23:05 -0300

    Adciionada Tabela Formatada e responsiva

--- Modules/ControleUsuario/Resources/views/consulta.blade.php
+++ Modules/ControleUsuario/Resources/views/consulta.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Cadastrar')
 @section('content')
 
-<div class="row justify-content-center">
+<div class="row justify-content-center justify-items-center ">
 
    <div class="col col-sm-10 col-md-8">
 
@@ -11,10 +11,10 @@
                 <h5 class="card-title">Buscar Usuário</h5>
             </div>
 
-        <div class="card-body">
+            <div class="card-body">
+            {!!Form::open(['route'=>'usuario.listar', 'method'=>'post']) !!}
 
-         {!!Form::open(['route'=>'usuario.listar', 'method'=>'post']) !!}
-               <div class="row align-items-end">
+                <div class="row align-items-end">
                   <div class="col-8">
                      <div class="form-group">
                         {!! form::text('nome',null,['placeholder'=>"Insira o nome aqui",'class'=>"form-control"]) !!}
@@ -43,8 +43,9 @@
                   </div>
                </div>
 
-               <div class="row pb-3 align-items-center">
-                   <div class="col d-flex justify-content-between">
+               <div class="row pb-3 align-items-center justify-content-center">
+                   <div class="col col-md-10 col-sm-10 d-flex justify-content-between">
+
                         <div class="form-check form-check-inline">
                             {!! Form::checkbox("adm", null,['class'=>'form-check-label'], false) !!}
                                 Administradores
@@ -60,8 +61,8 @@
                     </div>
                 </div>
 
-                <div class="row align-items-center">
-                    <div class="col d-flex justify-content-around ">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-sm-10 d-flex justify-content-around">
 
                         <div class="form-group">
                             <button type="submit" class="btn btn-success d-flex">
@@ -79,48 +80,45 @@
 
                 </div>
                 {!! Form::close() !!}
-        </div>
-    </div>
-        <div class="row">
-            <div class="col">
-                <table class="table table-dark table-responsive">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Primeiro</th>
-                            <th scope="col">Último</th>
-                            <th scope="col">Nickname</th>
-                        </tr>
-                    </thead>
-                              <tbody>
-                                <tr>
-                                  <th scope="row">1</th>
-                                  <td>Mark</td>
-                                  <td>Otto</td>
-                                  <td>@mdo</td>
+
+                <div class="row justify-content-center ">
+                    <div class="col-12">
+
+                        <table class="table table-sm
+                            table-dark table-striped table-bordered text-center justify-content-center">
+                        <thead>
+                            <tr>
+                                <th scope="col-3">ID</th>
+                                <th scope="col-3">Nome</th>
+                                <th scope="col-5">E-mail</th>
+                                <th scope="col">Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">3</th>
+                                    <td>Larry</td>
+                                    <td>Larry@email.com</td>
+                                    <td>
+                                        <div class="d-flex justify-content-center">
+                                            <input type="hidden" value="20">
+                                            <button type="reset" class="btn btn-warning d-flex">
+                                                <i class="material-icons mr-2">brush</i> Editar
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
-                                <tr>
-                                  <th scope="row">2</th>
-                                  <td>Jacob</td>
-                                  <td>Thornton</td>
-                                  <td>@fat</td>
-                                </tr>
-                                <tr>
-                                  <th scope="row">3</th>
-                                  <td>Larry</td>
-                                  <td>the Bird</td>
-                                  <td>@twitter</td>
-                                </tr>
-                              </tbody>
-                            </table>
+                        </tbody>
+                    </table>
+                    </div>
+                 </div>
 
-                </div>
+            </div>{{--  Fecha Card-Body  --}}
 
+        </div> {{--  Fecha o Card  --}}
 
-            </div>
-        </div>
+    </div> {{--  Fecha a Coluna inteira  --}}
 
-        </div>
-</div>
+</div> {{--  :Fecha a linha  --}}
 
 @endsection
