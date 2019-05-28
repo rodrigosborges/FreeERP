ebead5f2fd31c2f6e2ffaced6b33336e537bbb9b
FARID <okada.hikao@gmail.com>
2019-05-27 22:12:43 -0300

    front novaConta Ok

--- Modules/ContaAPagar/Resources/views/_formConta.blade.php
+++ Modules/ContaAPagar/Resources/views/_formConta.blade.php
@@ -1,3 +1,65 @@
 <div class="row">
-asdadsad
-</div>
+        <div class="col-md-2 d-flex justify-content-center align-items-center">
+            <a class="btn btn-primary" data-toggle="collapse" href="#collapseInputs" role="button" aria-expanded="false"
+                aria-controls="collapseInputs collapseSave">
+                +Conta
+            </a>
+        </div>
+        <div class="collapse" id="collapseSave">
+            <a class="btn btn-primary" role="button">
+                Salvar
+            </a>
+        </div>
+        <div class="col-md-10">
+            <div class="collapse" id="collapseInputs">
+                <div class="card card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <i class="material-icons input-group-text" id="basic-addon1">
+                                        sort
+                                    </i>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Descrição" aria-label="descricao"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <i class="material-icons input-group-text" id="basic-addon1">
+                                        date_range
+                                    </i>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Vencimento" aria-label="dataVencimento"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <i class="material-icons input-group-text" id="basic-addon1">
+                                        date_range
+                                    </i>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Pagamento" aria-label="dataVencimento"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <i class="material-icons input-group-text" id="basic-addon1">
+                                        attach_money
+                                    </i>
+                                </div>
+                                <input type="text" class="form-control" placeholder="Valor" aria-label="dataVencimento"
+                                    aria-describedby="basic-addon1">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
