e83d95605d2ba7090daddadaedd2687f9bab9b64
Denise Lopes <denise@kosloski.com.br>
2019-06-07 18:05:20 -0300

    atualizações na view de login

--- Modules/ControleUsuario/Resources/views/login.blade.php
+++ Modules/ControleUsuario/Resources/views/login.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="row justify-content-center align-items-center" style="height:100%">
+<!--<div class="row justify-content-center align-items-center" style="height:100%">
     <div class="col-xm-12 col-sm-10 col-md-8 col-lg-6">
         {!!Form::open(['route'=>'validar.login', 'method'=>'post']) !!}
         <p>Acesso ao sistema</p>
@@ -23,5 +23,36 @@
         </div>
         {!!Form::close()!!}
     </div>
+</div> -->
+
+<div class="row justify-content-center text-center" id="container">
+    <div class="col-xm-12 col-sm-10 col-md-8 col-lg-6">
+        <div class="card ">
+            <div class="card-header">
+                <h4>Acessar</h4>
+            </div>
+            <div class="card-body">
+                <div class="form col-sm-10  ">
+                    {!!Form::open(['route'=>'validar.login', 'method'=>'post']) !!}
+               
+                    <div class="form-group">
+                    {!! Form::text('email',null,['class'=>'form-control','placeholder'=>'Email']) !!}
+                            <span class="errors alert-danger"> {{ $errors->first('email') }} </span>
+                        </div>
+                        <div class="form-group">
+                        {!! Form::password('password', ['class'=>'form-control','placeholder'=>'Senha']) !!}
+                            <span class="errors alert-danger"> {{ $errors->first('password') }} </span>
+                        </div>
+                        
+                        {!!Form::submit('Entrar', ['class'=>'btn btn-primary d-flex align-items-center'])!!}
+                    {!!Form::close()!!}
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 @endsection
