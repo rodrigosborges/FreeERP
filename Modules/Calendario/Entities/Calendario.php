4b2a1d824d78462d842f08c7c40e6a0306514f5b
hugocuba <HgsllscB31038816>
2019-06-14 16:48:24 -0300

    Arquivos de models e migrations criados

---
+++ Modules/Calendario/Entities/Calendario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Calendario\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Calendario extends Model
+{
+    protected $fillable = [];
+}
---
+++ Modules/Calendario/Entities/Evento.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Calendario\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{
+    protected $fillable = [];
+}
