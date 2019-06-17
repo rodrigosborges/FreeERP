516e9776264953fa7be4a9ede9c843dc43652f48
alvesrafael17 <32360540+alvesrafael17@users.noreply.github.com>
2019-06-17 19:23:52 -0300

    Update CategoriaReceberSeeder.php

--- database/seeds/CategoriaReceberSeeder.php
+++ database/seeds/CategoriaReceberSeeder.php
@@ -7,6 +7,30 @@ class CategoriaReceberSeeder extends Seeder
     public function run()
     {
         DB::table('categoria_receber')->insert([
+          [
+            'nome' => 'Energia Elétrica',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Internet',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Telefone',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'TV por assinatura',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Água',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Impostos',
+            'ativo' => 1
+          ]
         ]);
     }
 }
