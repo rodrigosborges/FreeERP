08cb1302fbce3b99986cbbd9cdfbb0baf42ce795
alvesrafael17 <32360540+alvesrafael17@users.noreply.github.com>
2019-06-17 19:24:57 -0300

    Create FormaPagamentoReceberSeeder.php

---
+++ database/seeds/FormaPagamentoReceberSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use Illuminate\Database\Seeder;
+class FormaPagamentoReceberSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('forma_pagamento_receber')->insert([
+        [
+          'nome' => 'Dinheiro',
+          'taxa' => 0,
+          'prazo_recebimento' => 0,
+          'ativo' => 1
+        ]
+      ]);
+    }
+}
