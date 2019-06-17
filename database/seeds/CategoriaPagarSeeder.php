513154aa3abe97aa64da5f584326e716f6f1fea2
alvesrafael17 <32360540+alvesrafael17@users.noreply.github.com>
2019-06-17 19:35:02 -0300

    adicionando seeds

--- database/seeds/CategoriaPagarSeeder.php
+++ database/seeds/CategoriaPagarSeeder.php
@@ -7,28 +7,22 @@ class CategoriaPagarSeeder extends Seeder{
     {
         DB::table('categoria_pagar')->insert([
           [
-            'nome' => 'Energia Elétrica',
-            'ativo' => 1
+            'nome' => 'Energia Elétrica'
           ],
           [
-            'nome' => 'Internet',
-            'ativo' => 1
+            'nome' => 'Internet'
           ],
           [
-            'nome' => 'Telefone',
-            'ativo' => 1
+            'nome' => 'Telefone'
           ],
           [
-            'nome' => 'TV por assinatura',
-            'ativo' => 1
+            'nome' => 'TV por assinatura'
           ],
           [
-            'nome' => 'Água',
-            'ativo' => 1
+            'nome' => 'Água'
           ],
           [
-            'nome' => 'Impostos',
-            'ativo' => 1
+            'nome' => 'Impostos'
           ]
         ]);
     }
