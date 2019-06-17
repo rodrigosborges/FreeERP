0e340aca853b979e214fcb5f94ca384f49e65017
Rafael <alvesrafael17@hotmail.com>
2019-06-17 19:17:40 -0300

    criado seeders

--- Modules/ContaAReceber/Database/Seeders/ContaAReceberDatabaseSeeder.php
+++ Modules/ContaAReceber/Database/Seeders/ContaAReceberDatabaseSeeder.php
@@ -7,15 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContaAReceberDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        // $this->call("OthersTableSeeder");
-    }
+  public function run(){
+      DB::table('categoria_receber')->insert([
+          [
+            'nome' => 'Energia Elétrica',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Internet',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Telefone',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'TV por assinatura',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Água',
+            'ativo' => 1
+          ],
+          [
+            'nome' => 'Impostos',
+            'ativo' => 1
+          ]
+      ]);
+      DB::table('forma_pagamento_receber')->insert([
+        [
+          'nome' => 'Dinheiro',
+          'taxa' => 0,
+          'prazo_recebimento' => 0,
+          'ativo' => 1
+        ]
+      ]);
+  }
 }
