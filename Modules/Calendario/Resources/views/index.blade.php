b6513dd8a1942b035667cb5690412f1f91f02b8d
hugocuba <HgsllscB31038816>
2019-06-20 20:16:32 -0300

    Adicionado colorselect ao index

--- Modules/Calendario/Resources/views/index.blade.php
+++ Modules/Calendario/Resources/views/index.blade.php
@@ -17,7 +17,53 @@
                 </div>
                 <div class="modal-body">
                     <form action="{{route('eventos.criar')}}" id="eventoForm" method="post">
-
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-11">
+                                    <input type="text" class="form-control" id="eventoTitulo" placeholder="Título">
+                                </div>
+                                <div class="col-1">
+                                    <select id="eventoCor">
+                                        <option value="A0522D" data-color="#A0522D">sienna</option>
+                                        <option value="CD5C5C" data-color="#CD5C5C">indianred</option>
+                                        <option value="FF4500" data-color="#FF4500">orangered</option>
+                                        <option value="DC143C" data-color="#DC143C">crimson</option>
+                                        <option value="FF8C00" data-color="#FF8C00">darkorange</option>
+                                        <option value="C71585" data-color="#C71585">mediumvioletred</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col-6">
+                                    <input type="date" class="form-control" id="eventoDataInicio" placeholder="Início">
+                                </div>
+                                <div class="col-5">
+                                    <input type="date" class="form-control" id="eventoDataInicio" placeholder="Início">
+                                </div>
+                                <div class="col-1">
+                                    <i class="material-icons">alarm</i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-row">
+                                <div class="col">
+                                    <input type="time" class="form-control" id="eventoHoraInicio">
+                                </div>
+                                <div class="col">
+                                    <input type="time" class="form-control" id="eventoHoraFim">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <select id="eventoAgenda" class="form-control">
+                                <option value="Pessoal">Pessoal</option>
+                                <option value="Pessoal">Pessoal</option>
+                                <option value="Pessoal">Pessoal</option>
+                            </select>
+                        </div>
                     </form>
                 </div>
                 <div class="modal-footer">
@@ -41,6 +87,8 @@
           href="{{Module::asset(config('calendario.id').':fullcalendar-4.2.0/packages/list/main.min.css')}}">
     <link rel="stylesheet" type="text/css"
           href="{{Module::asset(config('calendario.id').':fullcalendar-4.2.0/packages/bootstrap/main.min.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{Module::asset(config('calendario.id').':bootstrap-colorselector-0.2.0/css/bootstrap-colorselector.css')}}">
     @parent
 @endsection
 
@@ -51,7 +99,14 @@
     <script src="{{Module::asset(config('calendario.id').':fullcalendar-4.2.0/packages/timegrid/main.min.js')}}"></script>
     <script src="{{Module::asset(config('calendario.id').':fullcalendar-4.2.0/packages/list/main.min.js')}}"></script>
     <script src="{{Module::asset(config('calendario.id').':fullcalendar-4.2.0/packages/core/locales/pt-br.js')}}"></script>
+    <script src="{{Module::asset(config('calendario.id').':bootstrap-colorselector-0.2.0/js/bootstrap-colorselector.js')}}"></script>
     <script>
+        $('#eventoCor').colorselector({
+            callback: function (value, color, title) {
+
+            }
+        });
+
         document.addEventListener('DOMContentLoaded', function () {
             var calendarEl = document.getElementById('calendario');
 
