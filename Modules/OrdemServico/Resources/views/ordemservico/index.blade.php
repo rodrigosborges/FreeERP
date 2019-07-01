525589cd90e30683e70aed2da978291aad398f39
Rebeca  Castilho <becacastilhocamargo@gmail.com>
2019-07-01 13:18:43 -0300

    confirmação de delete ordem  de serviço

--- Modules/OrdemServico/Resources/views/ordemservico/index.blade.php
+++ Modules/OrdemServico/Resources/views/ordemservico/index.blade.php
@@ -32,14 +32,11 @@
                 <td>{{$os->descricao_problema}}</td>
                 <td>{{$os->prioridade}}</td>
                 <td>
-                    <form action="{{url('ordemservico/os',[$os->id])}}" method="POST">
-                        {{method_field('DELETE')}}
-                        {{ csrf_field() }}
                         <a class="btn btn-outline-warning btn-sm" href='{{ url("ordemservico/os/$os->id/") }}'>Detalhes</a>
                         <a class="btn btn-outline-info btn-sm" href='{{ url("ordemservico/os/$os->id/edit") }}'>Editar</a>
                         <a class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#definir-prioridade">Prioridade</a>
                         <a class="btn btn-outline-dark btn-sm" href='{{ url("ordemservico/os/pdf")}}'>PDF</a>
-                        <input type="submit" class="btn btn-outline-danger btn-sm" value="Delete" />
+                        <a class="btn btn-outline-danger btn-sm" id='delete'  data-toggle="modal" data-id={{$os->id}} data-target="#verifica-delete"> Delete </a>
                     </form>
                 </td>
             </tr>
@@ -48,7 +45,7 @@
     </table>
 </div>
 
-<!-- Modal -->
+<!-- Modals -->
 <div class="modal fade" id="definir-prioridade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -59,7 +56,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                {{ Form::open(array('id' => 'form' , 'method'=>'put')) }}
+                {{ Form::open(array('id' => 'form-prioridade' , 'method'=>'put')) }}
                 {{Form::token()}}
 
 
@@ -81,6 +78,34 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="verifica-delete" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cancelar">
+                </button>
+            </div>
+            <div class="modal-body">
+                <h5>Tem certeza que deseja excluir essa OS?</h5>
+                <p class="text-secondary">Você poderá recuperar ordem de serviço na aba de reativação </p>
+                <form id="form-delete" method="POST">
+                {{method_field('DELETE')}}
+                        {{ csrf_field() }}
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                    {{Form::submit( 'Sim',array('class'=>"btn btn-primary") )}}
+                </div>
+                {{ Form::close() }}
+
+
+            </div>
+        </div>
+    </div>
+
+<!--- Paginaçao --->
 <div class="row justify-content-center">
     {{ $data['ordem_servico']->links() }}
 </div>
@@ -94,11 +119,16 @@
             var id = linha.find("td:eq(0)").text().trim();
             var prioridade = linha.find("td:eq(3)").text().trim();
            
-            $("#form").attr("action", '/ordemservico/os/' + id);
+            $("#form-prioridade").attr("action", '/ordemservico/os/' + id);
             $("#campo").remove();
-            $('#prioridade').append("<select id='campo' required name='prioridade' class='custom-select mr-sm-2'><option value='3'> Baixa </option><option value='2'> Média </option><option value='1'> Alta </option><option value='0'> A Definir </option></select>");
+            $('#prioridade').append("<select id='campo' required name='prioridade' class='custom-select mr-sm-2'><option value='0' disabled> A Definir </option><option value='3'> Baixa </option><option value='2'> Média </option><option value='1'> Alta </option></select>");
             $('#campo').val(prioridade);
         });
+    });
+
+    $("#verifica-delete").on('shown.bs.modal', function () {
+        var id = $('#delete').attr('data-id');
+        $("#form-delete").attr("action",'/ordemservico/os/'+ id);
     });
 </script>
 @endsection
