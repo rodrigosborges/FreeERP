f9ed65af7dd1efe6ba6f01f311a9d16a12da651b
Rafael Alvesfael <alvesrafael17@hotmail.com>
2019-08-20 21:27:13 -0300

    Iniciando modulo de clientes

---
+++ Modules/Cliente/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('cliente::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('cliente.name') !!}
+    </p>
+@stop
