59676241ba73e75cc45e63eb7b7b3a99cd784c18
FubsAlves <fabianoxtx@gmail.com>
2019-08-21 22:02:57 -0300

    Base de formulário cliente criada

--- resources/views/formcliente.blade.php
+++ resources/views/formcliente.blade.php
@@ -1,38 +1,83 @@
-@extends('template')
-@section('title', 'Exemplo')
+@extends('template') @section('title', 'Exemplo') @section('content')
 
-@section('content')
-    <div class="row justify-content-center align-items-center" style="height:100%">
-        <div class="col-xm-12 col-sm-10 col-md-8 col-lg-6">
-            <form>
-            <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                <label class="btn btn-secondary active">
-                    <input type="radio" name="pessoa_fisica" id="pessoa_fisica" autocomplete="off" checked> Pessoa Fisica
-                </label>
-                <label class="btn btn-secondary">
-                    <input type="radio" name="pessoa_juridica" id="pessoa_juridica" autocomplete="off"> Pessoa Juridica
-                </label>
-                </div>
-                
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <input type="text" class="form-control" id="nome" placeholder="Ex: Raimundo Nonato">
-                </div>
-                
-                <div class="form-group">
-                    <label for="numero_documento">Número de Documentação</label>
-                    <input type="text" class="form-control" id="numero_documento" placeholder="Ex: 461.305.874-55">
-                </div>
-                <div class="form-group">
-                    <label for="comprovante">Comprovante</label>
-                    <input type="text" class="form-control" id="comprovante" placeholder="Ex: 461.305.874-55">
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>    
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <div class="container">
+                <form>
+                    <div class='row py-3'>
+                        <h2>Dados Cadastrais</h2>
+                    </div>
+                    <div class="row py-3">
+                        <div class="form-group col-6">
+                            <label for="nome" class="col-form-label col-form-label-lg h6">Nome:</label>
+                            <input type="email" class="form-control" id="nome">
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="email" class="col-form-label col-form-label-lg h6">E-mail:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <h2>Documentação</h2>
+                    </div>
+                    <div class="row py-3">
+                        <div class="form-group col-6">
+                            <label for="numero_documento" class="col-form-label col-form-label-lg h6">Número de Documento:</label>
+                            <input type="text" class="form-control" id="numero_documento" placeholder="Ex: 451.658.200-50">
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="tipo_documento" class="col-form-label col-form-label-lg h6">Tipo de Documento:</label>
+                            <select class="custom-select" id="tipo_documento">
+                  
+                  <option value="1" selected>CPF</option>
+                  <option value="2">CNPJ</option>
+                  
+                </select>
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <div class="form-group col-4">
+                            <label for="comprovante_documento" class="col-form-label col-form-label-lg h6">Comprovante de Documentação:</label>
+                            <input type="file" class="form-control-file" id="comprovante_documento">
+                            <small id="comprovante_help" class="form-text text-muted">Apenas arquivos com as extensões: jpg, png, pdf</small>
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <h2>Endereço</h2>
+                    </div>
+                    <div class="row py-3">
+                        <div class="form-group col-2">
+                            <label for="cep" class="col-form-label col-form-label-lg h6">Cep:</label>
+                            <input type="cep" class="form-control" id="cep">
+                        </div>
+                        <div class="form-group col-9">
+                            <label for="logradouro" class="col-form-label col-form-label-lg h6">Logradouro:</label>
+                            <input type="logradouro" class="form-control" id="logradouro">
+                        </div>
+                        <div class="form-group col-1">
+                            <label for="numero" class="col-form-label col-form-label-lg h6 text-left">Número:</label>
+                            <input type="numero" class="form-control" id="numero">
+                        </div>
+                    </div>
+                    <div class="row py-3">
+                        <div class="form-group col-4">
+                            <label for="bairro" class="col-form-label col-form-label-lg h6">Bairro:</label>
+                            <input type="bairro" class="form-control" id="bairro">
+                        </div>
+                        <div class="form-group col-4">
+                            <label for="cidade" class="col-form-label col-form-label-lg h6">Cidade:</label>
+                            <input type="cidade" class="form-control" id="cidade">
+                        </div>
+                        <div class="form-group col-4">
+                            <label for="complemento" class="col-form-label col-form-label-lg h6 text-left">Complemento:</label>
+                            <input type="complemento" class="form-control" id="complemento">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
         </div>
     </div>
+</div>
 @endsection
