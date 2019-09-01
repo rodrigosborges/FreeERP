46a96e5a38babe7d340e3e2c8fb33d85039494f1
Denise Lopes <denise@kosloski.com.br>
2019-09-01 17:00:33 -0300

    validação front-end

--- Modules/Funcionario/Resources/views/pagamentos/form.blade.php
+++ Modules/Funcionario/Resources/views/pagamentos/form.blade.php
@@ -6,28 +6,36 @@
 
 @section('body')
 <form action="{{ $data['url'] }}" id="form" method="POST" enctype="multipart/form-data">
+
     {{ csrf_field() }}
+
+    <!--Funcionario -->
     <div class="row">
         <div class="input-group col-md-12">
-
             <div class="input-group-prepend">
                 <span class="input-group-text">
                     <i class="material-icons">person</i>
                 </span>
             </div>
+            <!--Select Funcionario -->
             <select class="custom-select" id="funcionario">
-                <!--Select Funcionario -->
                 <option selected value="-1">Selecione um Funcionário</option>
                 @foreach($funcionarios as $funcionario)
                 <option value="{{ $funcionario->id }}">{{ $funcionario->nome }}</option>
                 @endforeach
             </select>
+            <!--FIM Select Funcionario -->
             <span class="errors"> </span>
         </div>
     </div>
+    <!--FIM Funcionario -->
+
     <br>
 
+    <!--Cargo e Emissão -->
+
     <div class="row">
+        <!--Cargo -->
         <div class="form-group mb-5 col-md-6">
             <label class="control-label" for="inputGroupSelect01">Cargo</label>
             <div class="input-group">
@@ -36,12 +44,16 @@
                         <i class="material-icons">work</i>
                     </span>
                 </div>
+                <!--Select Cargo -->
                 <select class="custom-select" id="cargos">
-                    <option value='0'>Selecione</option>
-
+                    <option value='-1'>Selecione</option>
                 </select>
+                <!--FIM Select Cargo -->
             </div>
         </div>
+        <!--FIM Cargo -->
+
+        <!--Emissao -->
         <div class="form-group mb-5 col-md-6">
             <label class="control-label" for="emissao">Emissão</label>
             <div class="input-group">
@@ -53,22 +65,11 @@
                 <input type="date" class="form-control required" name="emissao" id="emissao">
             </div>
         </div>
-
+        <!--FIM Emissao -->
     </div>
-    <!--<div class="form-group col-md-12">
-            <label for="nome" class="control-label">Nome do funcionario</label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <i class="material-icons">person</i>
-                    </span>
-                </div>
-                <input type="text" name="nome" id="nome" class="form-control required" value="" disabled>
-            </div>
-            <span class="errors"> </span>
-        </div>
-    </div>-->
+    <!--FIM Cargo e Emissão -->
 
+    <!--opção Pagamento e Horas extras -->
     <div class="row">
         <!--Opções Pagamento -->
         <div class="form-group mb-5 col-md-6">
@@ -80,15 +81,17 @@
                     </span>
                 </div>
                 <select class="custom-select" id="opcao-pagamento">
-
+                    <option value="-1">Selecione</option>
                     <option selected value="1">Salário</option>
                     <option value="2">Adiantamento</option>
-                    <option value="2">Férias</option>
+                    <option value="3">Férias</option>
                 </select>
             </div>
         </div>
         <!--Fim Opções Pagamento -->
+
         <br>
+
         <!-- Horas Extras-->
         <div class="form-group col-md-6">
             <label for="salario" class="control-label"> Horas Extras</label>
@@ -98,13 +101,13 @@
                         <i class="material-icons">access_time</i>
                     </span>
                 </div>
-                <input type="text" name="horas_extras" id="h_extra" class="form-control required money">
+                <input type="text" name="horas_extras" id="horas_extras" class="form-control required money">
             </div>
             <span class="errors"> </span>
         </div>
         <!-- Fim Horas Extras-->
-
     </div>
+    <!--FIM opção Pagamento e Horas extras -->
 
     <div class="row">
         <!-- Valor-->
@@ -137,7 +140,9 @@
         </div>
         <!-- Fim Adicional Noturno-->
     </div>
+
     <br>
+
     <div class="row">
         <!-- Faltas-->
         <div class="form-group col-md-6">
@@ -193,8 +198,13 @@
 </div>
 @endsection
 
+<!--------------JQUERY------------->
+<!--author: Denise -->
+
 @section('script')
+
 <script src="{{Module::asset('funcionario:js/views/cargo/form.js')}}"></script>
+
 <script>
     //Variaveis Globais
     var total;
@@ -203,77 +213,171 @@
     var falta
     var temporaria
     var adicional
+
+    //Função desabilitar: inputs conforme preenchimento do campo
+
     $(document).ready(function(e) {
-
-        function recalcular(tipo) {
-            var temp
-
-
-
-
-            if (tipo == 1) {
-                if ($('#faltas').val() == ""&& $('#faltas').val()==0) {
-                    adicional = parseFloat($('.adicional1').val())
-                    temp = total + adicional;
-                    alert(adicional + "- " + temp)
-                    $('#total').val(temp)
-                    
-                } else {
-                    temp = total + parseFloat($('.adicional1').val());
-                    $('#total').val(temp)
-                }
-                console.log("adicional:" + adicional + " - temp:" + temp + " total:" + total)
-
-            }
-            if (tipo == 2) {
-                if ($("#faltas").val() != "") {
-
-                    falta = parseInt($('#faltas').val())
-                    var valor_hora = parseFloat(selectedCargo.salario) / parseFloat(selectedCargo.horas_semanais)
-                    var horas_dia = (selectedCargo.horas_semanais / 5)
-                    var horas_trabalhada = selectedCargo.horas_semanais - (horas_dia * falta)
-                    var desconto = (parseFloat(selectedCargo.salario) / 30) * (horas_dia * falta)
-                    temp = total - desconto
-                    $('#total').val(temp)
-                    console.log("valor hora:" + valor_hora + "- horas dia:" + horas_dia + "-horas trabalhadas:" + horas_trabalhada + " - Desconto :" + desconto + "vat total:" + total + "total_temp:" + temp)
-                }
-            }
-
-        }
-
-        $('.adicional1').blur(function() {
-            recalcular(1)
-        })
-        $('#faltas').change(function() {
-            recalcular(2)
-        })
+////////////MAIN///////////////
+        //Chamadas de funções:funções
         // quando tirado o foco do select de funcionario ele dispara a função
         //a função faz uma requisição ajax para a pagina buscaCargos
-        // autor: Denise Lopes
-        $('#funcionario').change(function() {
-            buscaFuncionario()
-        })
         // select de cargo
         // autor: Denise Lopes
-        $('#cargos').change(function() {
-            buscaSalario()
+
+
+        //verificação para habilitar busca funcionario
+        $('#funcionario').change(function() {
+            if ($('#funcionario').val() == -1) {
+                desabilitar(true)
+                $('#valor').val('')
+                $('#inss').val('')
+                $('#total').val('')
+            } else
+                buscaFuncionario()
         })
+
+        //No selecione ele não permanece com os dados do funcionario
+        $('#cargos').change(function() {
+
+
+            if ($('#cargos').val() != -1) {
+                buscaSalario()
+                $('#emissao').val('')
+            } else {
+                desabilitar(true)
+                $('#funcionario').attr('disabled', false)
+                $('#cargos').attr('disabled', false)
+                $('#valor').val('')
+                $('#inss').val('')
+                $('#total').val('')
+
+            }
+        })
+
         $('#opcao-pagamento').change(function() {
+
             opcaoPagamento()
         })
 
+        $('#emissao').change(function() {
+            $('#opcao-pagamento').attr('disabled', false)
+            desabilitar(false)
+        })
+        // Mantém os inputs em cache:
+        var inputs = $('input');
 
+        // Chama a função de verificação quando as entradas forem modificadas
+        // Usei o 'keyup', mas 'change' ou 'keydown' são também eventos úteis aqui
+        inputs.on('keyup', verificarInputs);
+
+        verificarInputs()
+////////////FIM "MAIN"///////////////
+
+        //FUNÇÕES
+        function verificarInputs() {
+            var preenchidos = true; // assumir que estão preenchidos
+            inputs.each(function() {
+                // verificar um a um e passar a false se algum falhar
+                // no lugar do if pode-se usar alguma função de validação, regex ou outros
+                console.log(this)
+                if (!this.value) {
+                    preenchidos = false;
+                    // parar o loop, evitando que mais inputs sejam verificados sem necessidade
+                    //return false;
+                }
+
+            });
+            // Habilite, ou não, o <button>, dependendo da variável:
+            $('button').prop('disabled', !preenchidos); // 
+            if (preenchidos) {
+                calcular()
+            }
+
+        }
+        //calcula salario, hora_extra, adicional noturno, inss
+
+        function calcular() {
+            console.log("Valor salario :" + selectedCargo.salario + "hora Extra:" + $('#horas_extras').val() + "Adicional noturno:" + $('.adicional1').val() + " Faltas:" + $('#faltas').val())
+            var salario = parseFloat(selectedCargo.salario)
+            var horas_extras = parseFloat($('#horas_extras').val())
+            var valor_dia = salario / 20
+            var horas_dias = parseFloat(selectedCargo.horas_semanais / 5)
+            var valor_hora_extra = (valor_dia / 8) * horas_extras
+            var adicional = parseFloat($('.adicional1').val())
+            var faltas = parseFloat($('#faltas').val())
+            var inss = parseFloat($('#inss').val())
+            var desconto = salario / 30 * (faltas * horas_dias)
+
+            console.log("desconto:" + -desconto)
+            var temp = total;
+
+            //do total ele efetua os descontos e soma os extras
+            temp -= desconto
+            temp += (valor_hora_extra + adicional)
+            console.log(temp)
+            $('#total').val(temp.toFixed(2)) //to fixed é para arrumar as casas decimais
+            console.log("valor hora:" + valor_dia / 8)
+
+        }
+
+        //FIM - calcula salario, hora_extra, adicional noturno, inss
+
+
+        desabilitar(true)
+
+        function desabilitar(opcao) {
+
+            $('#cargos').attr('disabled', opcao);
+            $('#cargos').attr('disabled', opcao);
+            $('#emissao').attr('disabled', opcao);
+            $('#opcao-pagamento').attr('disabled', opcao);
+            $('#horas_extras').attr('disabled', opcao);
+            $('.adicional1').attr('disabled', opcao);
+            $('#faltas').attr('disabled', opcao);
+        }
 
     })
     // autor: Denise Lopes
     function opcaoPagamento() {
-        if ($('#opcao-pagamento').val() != 1) {
+        if ($('#opcao-pagamento').val() == -1) {
             $('#valor').val('')
             $('#inss').val('')
             $('#total').val('')
         } else {
             buscaSalario()
         }
+    }
+
+    function calculaInss(data) {
+        var salario;
+
+        if (data != null && data != "") {
+            salario = parseFloat(data)
+
+            formula = (salario * 8) / 100;
+            // Aliquota minima 
+            $('#valor').val(salario.toFixed(2))
+            if (salario <= 1751.81) {
+                inss = formula;
+
+            } else if (data > 1751.81) {
+                inss = formula;
+
+            } else {
+                inss = formula;
+            }
+            total = salario - inss;
+            $('#total').val(total.toFixed(2))
+
+            $('#inss').val(inss.toFixed(2));
+        } else {
+
+            $('#valor').val("")
+            $('#inss').val("")
+            $('#total').val("")
+
+        }
+
     }
 
     function buscaSalario() {
@@ -288,34 +392,19 @@
             //data é igual a salário
         }).done(function(data) {
 
-            var salario;
+            $('#emissao').attr('disabled', false)
+            if ($('#opcao-pagamento').val() == 2) {
+                var valor = parseFloat(data) * 0.4
 
-            if (data != null && data != "") {
-                var salario = parseFloat(data)
+                calculaInss(valor)
+                console.log("Adiantamento:" + valor)
+            } else
+                calculaInss(data)
 
-                formula = (salario * 8) / 100;
-                // Aliquota minima 
-                $('#valor').val(salario)
-                if (salario <= 1751.81) {
-                    inss = formula;
-
-                } else if (data > 1751.81) {
-                    inss = formula;
-
-                } else {
-                    inss = formula;
-                }
-                total = salario - inss;
-                $('#total').val(total)
-
-                $('#inss').val(inss);
-            } else {
-
-                $('#valor').val("")
-                $('#inss').val("")
-                $('#total').val("")
-
-            }
+            // $('#opcao-pagamento').attr('disabled',false)
+            // $('#horas_extras').attr('disabled',false)
+            // $('.adicional1').attr('disabled',false)
+            // $('#faltas').attr('disabled',false)
 
 
             console.log()
@@ -335,6 +424,7 @@
 
             //alert(desconto)
             //  
+
         }).fail(function() {
 
         })
@@ -359,7 +449,8 @@
             //após capturar os cargos ele faz um foreach nos cargos e adiciona em uma string 
             // que posteriormente é enviada para o select de cargos
             cargos = $.parseJSON(data);
-            string = " <option value='0'>Selecione</option>"
+            $('#cargos').attr('disabled', false)
+            string = " <option value='-1'>Selecione</option>"
             $.each(cargos, function(chave, valor) {
                 string += '<option value="' + valor.id + '">' + valor.nome + "</option>"
                 console.log(valor.horas_semanais);
@@ -371,6 +462,11 @@
 
             $('#cargos').html(string);
         }).fail(function() {})
+
+
+
+
+
     }
 </script>
 
