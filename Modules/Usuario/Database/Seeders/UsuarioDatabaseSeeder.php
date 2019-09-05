906b8983923b84784ecb1279925f6f2c1a0bad3a
Gabriel de Sousa Matsumura <sousa.matsumura@gmail.com>
2019-09-05 20:08:15 -0300

    Validação unique do CRUD de papel, no banco e no back, e seeder admin para usuario e papel

--- Modules/Usuario/Database/Seeders/UsuarioDatabaseSeeder.php
+++ Modules/Usuario/Database/Seeders/UsuarioDatabaseSeeder.php
@@ -4,17 +4,23 @@ namespace Modules\Usuario\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
-class UsuarioDatabaseSeeder extends Seeder
-{
+class UsuarioDatabaseSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        Model::unguard();
+    public function run(){
+        DB::table('usuario')->insert([
+            'apelido' => 'admin',
+            'avatar' => 'default.png',            
+            'email' => 'admin@freeerp.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        //Model::unguard();
 
         // $this->call("OthersTableSeeder");
     }
