736a99bc6b00df080a38c251adc1faf5cbc72a51
FubsAlves <fabianoxtx@gmail.com>
2019-09-10 22:24:56 -0300

    finalizado busca por cep, alterado dicionario de dados do formulario e criado validações

---
+++ public/modules/cliente/js/views/cliente/validations.js
@@ -0,0 +1,28 @@
+$('#form').validate({
+    rules: {
+        "cliente[nome]": {
+            maxlength: 100,
+        },
+        "cliente[nome_fantasia]": {
+            maxlength: 100,
+        },
+        "email[email]": {
+            maxlength: 50,
+        },
+        "endereco[cep]": {
+            digits: true
+        },
+        "endereco[logradouro]": {
+            maxlength: 50
+        },
+        "endereco[numero]": {
+            digits: true
+        },
+        "endereco[bairro]": {
+            maxlength: 50
+        },
+
+    },
+
+    messages: {}
+})
