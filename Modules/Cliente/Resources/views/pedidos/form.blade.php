b70c4b29edab4aadc9a390468d905b820a61d95c
Lais Rebello Tavares <rebello.tavares@gmail.com>
2019-09-10 22:20:52 -0300

    Formulario registro de compra

--- Modules/Cliente/Resources/views/pedidos/form.blade.php
+++ Modules/Cliente/Resources/views/pedidos/form.blade.php
@@ -4,99 +4,114 @@ Nova Compra - {{ $cliente->nome }}
 @endsection
 @section('body')
 
-<div class="container border">
 
     <form action="" class="">
-
-        <div class="form-group form-row">
-            <div class="col-3">
-            <label for="data-pedido">Data da Compra</label>
-                <input type="date" name="data-pedido" id="data-pedido" class="form-control" placeholder="Data da Compra" required>
-            </div>
-            <div class="col-3 pt-2">
-                <label for="num-pedido"></label>
-                <input type="text" name="num-pedido" id="numero" class="form-control" placeholder="Numero da Compra">
-            </div>
-        </div>
-        
-        
-        <div class="input-group">
-            <div class="input-group-prepend col-6">
-                <span class="input-group-text purple lighten-3" id="basic-text1">
-                    <i class="material-icons" aria-hidden="true">search</i></span>
-            
-                <select class="form-control" name="produto_id">
-                    @foreach ($produtos as $produto)
-                <option value="{{$produto->id}}">{{$produto->codigo}} - {{$produto->nome}} - R$ {{$produto->preco}}</option>
-                    @endforeach
-                </select>
-            </div>
-            
-            <div class="col col-2">
-                <input type="number" min="1" class="form-control" name="qtde" id="qtde" placeholder="Qtde">
-            </div>
-
-            <div class="col col-2 input-group">
-                <input type="number" min="0" class="form-control" name="desconto" id="desconto"  
-                                aria-label="0,0%" aria-describedby="basic-addon2" placeholder="Desconto">
-                    <div class="input-group-append ">
-                            <span class="input-group-text" id="basic-addon2">%</span>
+        <div class="row">
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>    
                     </div>
+                    <input type="date" name="data" class="form-control">
+                </div>                        
             </div>
 
-            <div class="col-2 ">
-                <button type="button" class="btn btn-primary" style="width: 100%;">Adicionar</button>
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                    </div>
+                    <input type="number" name="numero" placeholder="Numero da Compra" class="form-control">
+                </div>
+                
+            </div>
+
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                    </div>                    
+                    <input type="number" name="desconto" placeholder="Desconto da compra" class="form-control">
+                </div>
+            </div>
+            
+        </div>
+        
+        <div class="row">
+
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                    </div>
+                    <select name="produtos[][produto_id]" id="" class="form-control">
+                        @foreach($produtos as $produto)           
+                            <option value="{{$produto->id}}">{{$produto->nome}}</option>
+                        @endforeach        
+                    </select>                
+                </div>                            
+            </div>
+
+
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                    </div>
+                    <input type="text" class="form-control" name="produtos[][quantidade]" placeholder="Quantidade">
+                </div>                 
+            </div>
+
+            <div class="col-4 form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                    </div>
+                    <input type="text" class="form-control" name="produtos[][desconto]" placeholder="Desconto do Produto">
+                </div>  
             </div>
         
         </div>
+    
     </form>
-    <hr />
-    <div id="itens_adicionados" class="border"> 
-        <table class="table table-striped bordered text-center col-md-12" id="adicionados">
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Descricao</th>
-                    <th>Preco</th>
-                    <th>Qtde</th>
-                    <th>Desconto UN</th>
-                    <th>Vl Total</th>
-                    <th>Opções</th>   
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
-    </div>
-</div>
+    
+
+
+@section('js')
 <script>
 
-    var itens_compra = [];
+var itens_compra = [];
 
-    function add_item(){
+function add_item(){
 
-        var opt = $("[name='produto_id']");     
+    var opt = $("[name='produto_id'] option:selected");     
 
-        dados = opt.text().split("-");
-        novaTabela(dados);
+    dados = opt.text().split("-");
+    novaTabela(dados);
 
-        alert(opt.val() + " " + opt.text());
+    alert(opt.val() + " " + opt.text());
 
-        itens_compra.push(opt.val());
+    itens_compra.push(opt.val());
 
-    }
+}
 
-    $("#add").click(function(){
-        var opt = $("[name='produto_id']");
-        dados = opt.text().split("-");
+$("#add").click(function(){
+    var opt = $("[name='produto_id'] option:selected");     
+    console.log();
+   
+    var nome = opt.attr("data-nome");
+    var preco = opt.attr("data-preco");
+    var codigo = opt.attr("data-codigo")
 
-        var quantidade = $('#qtde').val();
-        var desconto = $('#desconto').val();
-         //CRIAR TBODY
-        var row = "<td>" +  dados + "</td></tr>" + quantidade + "</td><td>" + desconto +"</td><td>";
-        $("#adicionados tbody").append(row);
-    });
+    var quantidade = $('#qtde').val();
+    var desconto = $('#desconto').val();
+     //CRIAR TBODY
 
-
+    var row = "<tr><td>" +  codigo + "</td><td>" + nome + "</td><td>" + preco +"</td><td>" + quantidade + "</td><td>" + desconto + "</td></tr>";
+ 
+    $("#adicionados tbody").append(row);
+});
 
 </script>
+@endsection
 @endsection
