b5112f6064ea16f26480d53e4dc93d1b8e2cd305
Lais Rebello Tavares <rebello.tavares@gmail.com>
2019-09-11 21:17:02 -0300

    Formulario de cadastro do pedido

--- Modules/Cliente/Resources/views/pedidos/form.blade.php
+++ Modules/Cliente/Resources/views/pedidos/form.blade.php
@@ -10,7 +10,7 @@ Cadastro Nova Compra - {{ $cliente->nome }}
             <div class="col-4 form-group">
                 <div class="input-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>    
+                        <span class="input-group-text"><i class="material-icons">calendar_today</i></span>    
                     </div>
                     <input type="date" name="data" class="form-control">
                 </div>                        
@@ -19,7 +19,7 @@ Cadastro Nova Compra - {{ $cliente->nome }}
             <div class="col-4 form-group">
                 <div class="input-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                        <span class="input-group-text"><i class="material-icons">local_atm</i></span>
                     </div>
                     <input type="number" name="numero" placeholder="Numero da Compra" class="form-control">
                 </div>
@@ -29,48 +29,59 @@ Cadastro Nova Compra - {{ $cliente->nome }}
             <div class="col-4 form-group">
                 <div class="input-group">
                     <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                        <span class="input-group-text"><i class="material-icons">arrow_downward</i></span>
                     </div>                    
                     <input type="number" name="desconto" placeholder="Desconto da compra" class="form-control">
                 </div>
             </div>
             
         </div>
+        <hr>
         <div class="produtos">
             <div class="row produto ">
-                <div class="col-4 form-group">
+                <div class="col form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
                         </div>
                         <select name="produtos[][produto_id]" id="" class="form-control">
+                            <option value="" selected>Selecione o produto</option>
                             @foreach($produtos as $produto)           
                                 <option value="{{$produto->id}}">{{$produto->nome}}</option>
                             @endforeach        
                         </select>                
-                    </div>                            
+                    </div>
+
                 </div>
-                <div class="col-4 form-group">
+                <div class="col-3 form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                            <span class="input-group-text"><i class="material-icons">add_shopping_cart</i></span>
                         </div>
                         <input type="text" class="form-control" name="produtos[][quantidade]" placeholder="Quantidade">
                     </div>                 
                 </div>
-                <div class="col-4 form-group">
+                <div class="col-3 form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
+                            <span class="input-group-text"><i class="material-icons">trending_down</i></span>
                         </div>
-                        <input type="text" class="form-control" name="produtos[][desconto]" placeholder="Desconto do Produto">
+                        <input type="text" class="form-control" name="produtos[][desconto]" placeholder="Desconto">
                     </div>  
                 </div>
+                <div class="col-1 d-none">
+                    <button type="button" class="btn btn-danger btn-block excluir-produto"><strong>X</strong></button>
+                </div>
+                    
 
             </div>
         </div>
-        <button type="button" id="adicionar-produto" class="btn btn-success">Adicionar</button>
-        <button type="button" id="excluir-produto" class="btn btn-danger">Excluir</button>
+        <div class="text-center">
+            <button type="button" id="adicionar-produto" class="btn btn-success"><strong>+</strong></button>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Cadastrar compra</button>
+        
         
     
     </form>
@@ -79,15 +90,24 @@ Cadastro Nova Compra - {{ $cliente->nome }}
 
 @section('js')
 <script>
-     $('#adicionar-produto').click(function(){
+     $(document).on('click', '#adicionar-produto', function(){
+            $('.excluir-produto').parent().removeClass('d-none');
             var pedido = $(".produto").first().clone()
             pedido.find('select, input').val("")
             pedido.appendTo($(".produtos"))
         });
-        $('#excluir-produto').click(function(){
-            if ($(".produto").length > 1) {
-                $(".produto").last().remove()
+
+ 
+
+        $(document).on('click', '.excluir-produto',function(){
+            if($('.produto').length == 2){
+                $(this).closest('.produto').remove();
+                $('.excluir-produto').parent().addClass('d-none');
+            }else if($('.produto').length >= 2) {
+                $(this).closest('.produto').remove();
             }
+                
+            
         });
 
 
