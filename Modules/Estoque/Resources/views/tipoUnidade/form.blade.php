15cd20808499da227ba23fd3f2a9d15b77771911
Bruno Adriano <brunoa.f@hotmail.com>
2019-09-18 19:50:38 -0300

    view unidade

--- Modules/Estoque/Resources/views/tipoUnidade/form.blade.php
+++ Modules/Estoque/Resources/views/tipoUnidade/form.blade.php
@@ -1,7 +1,7 @@
 @extends('estoque::estoque.estoqueTemplate')
 @section('title', $data['titulo'])
 @section('body')
-<div class="card d-flex justify-content-center ">
+
     <form action="{{url($data['url'])}}" method="POST" class="">
         @csrf
         @if(isset($data['tipo']))
@@ -13,19 +13,16 @@
                 <input type="text" name='nome' id="nome" class="form-control" maxlength="45" value="{{($data['tipo'])?$data['tipo']->nome : ''}}" placeholder="ex: (Caixa, container, unidade)">
                 <p class=" alert-warning">{{$errors->first('nome')}} </p>
             </div>
-            <div class="col-6">
-                <div class="form-group">
-                    <label for="categoria_id">quantidade de itens</label>
-                    <input type="number" name="quantidade_itens" class="form-control" value="{{($data['tipo'])?$data['tipo']->quantidade_itens : ''}}">
-                    <p class="alert-warning">{{$errors->first('quantidade_itens')}}</p>
-                </div>
+            <div class="form-group col-6">
+                <label for="categoria_id">Quantidade de itens</label>
+                <input type="number" name="quantidade_itens" class="form-control" value="{{($data['tipo'])?$data['tipo']->quantidade_itens : ''}}">
+                <p class="alert-warning">{{$errors->first('quantidade_itens')}}</p>
             </div>
         </div>
-        <div class="row form-group justify-content-end ">
+        <div class="row mr-1" style="justify-content: flex-end;">
             <button type="submit" id="enviar" class="btn btn-primary">{{$data['button']}}</button>
         </div>
     </form>
-</div>
 
 
 @endsection
