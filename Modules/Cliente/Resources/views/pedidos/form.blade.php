c61de3d96ea5264237a85b9e2ab560e83d2587b5
Rafael Alves <alvesrafael17@hotmail.com>
2019-09-18 22:23:22 -0300

    arrumei documentos excluidos em merges anteriores

--- Modules/Cliente/Resources/views/pedidos/form.blade.php
+++ Modules/Cliente/Resources/views/pedidos/form.blade.php
@@ -5,75 +5,84 @@ Cadastro Nova Compra - {{ $cliente->nome }}
 @section('body')
 
 
-    <form action="" class="">
+    <form id="form" action="{{isset($pedido) ? '/pedido/'.$pedido->id : $cliente->id.'/pedido'}}" method="`POST">
+        @csrf
+        @if(isset($pedido)) 
+            @method('put')
+        @endif
         <div class="row">
-            <div class="col-4 form-group">
+            <div class="col-lg-4 col-md-12 form-group">
+                <label for="data">Data da compra:</label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="material-icons">calendar_today</i></span>    
                     </div>
-                    <input type="date" name="data" onfocusout="checkDate()" id="data" class="form-control" value="{{ isset($pedido->data) ? $pedido->data : old('data', '') }}">
+                    <input type="text" required name="data" id="data" class="form-control" value="{{ isset($pedido->data) ? $pedido->data : old('data', '') }}">
+                    
                 </div>                        
             </div>
 
-            <div class="col-4 form-group">
+            <div class="col-lg-4 col-md-6 col-sm-12 form-group">
+                <label for="numero">Numero da compra:</label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="material-icons">local_atm</i></span>
                     </div>
-                    <input type="number" name="numero" placeholder="Numero da Compra" class="form-control" value="{{ isset($pedido->numero) ? $pedido->numero : old('numero', '') }}">
+                    <input type="text" required name="numero" placeholder="Numero da Compra" class="form-control" value="{{ isset($pedido->numero) ? $pedido->numero : old('numero', '') }}">
                 </div>
                 
             </div>
 
-            <div class="col-4 form-group">
+            <div class="col-lg-4 col-md-6 col-sm-12 form-group">
+                <label for="desconto">Desconto da compra:</label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="material-icons">arrow_downward</i></span>
                     </div>                      
-                    <input type="number" name="desconto" placeholder="Desconto da compra" class="form-control" value="{{isset($pedido->desconto) ? $pedido->desconto : old('pedido', '')}}">
+                    <input type="text" required name="desconto" placeholder="Desconto da compra" class="form-control desconto" value="{{isset($pedido->desconto) ? $pedido->desconto : old('pedido', '')}}">
                 </div>
             </div>
             
         </div>
         <hr>
         <div class="produtos">
+            <h3>Produto(s)</h3>
+            
             <div class="row produto ">
-                <div class="col form-group">
+                <hr>
+                <div class="col-lg col-md form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="material-icons">format_list_numbered</i></span>
                         </div>
-                        <select name="produtos[0][produto_id] produto_id" id="" class="form-control">
+                        <select name="produtos[0][produto_id] produto_id" required id="" class="form-control">
                             <option value="" selected>Selecione o produto</option>
                             @foreach($produtos as $produto)           
                                 <option value="{{$produto->id}}">{{$produto->nome}}</option>
                             @endforeach        
                         </select>                
                     </div>
-
                 </div>
-                <div class="col-3 form-group">
+                <div class="col-lg-3 col-md-6 col-sm-11 form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="material-icons">add_shopping_cart</i></span>
                         </div>
-                        <input type="text" class="form-control produto_quantidade"  name="produtos[0][quantidade]" placeholder="Quantidade">
+                        <input type="text" required class="form-control produto_quantidade"  name="produtos[0][quantidade]" placeholder="Quantidade">
                     </div>                 
                 </div>
-                <div class="col-3 form-group">
+                <div class="col-lg-3 col-md-6 col-sm-11 form-group">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="material-icons">trending_down</i></span>
                         </div>
-                        <input type="text" class="form-control produto_desconto" name="produtos[0][desconto]" placeholder="Desconto">
+                        <input type="text" required class="form-control produto_desconto desconto" name="produtos[0][desconto]" placeholder="Desconto">
                     </div>  
                 </div>
-                <div class="col-1 d-none">
+                <div class="col-lg-1 col-sm-12 form-group d-none">
                     <button type="button" class="btn btn-danger btn-block excluir-produto"><strong>X</strong></button>
                 </div>
-                    
-
+                <hr>
             </div>
         </div>
         <div class="text-center">
@@ -81,51 +90,80 @@ Cadastro Nova Compra - {{ $cliente->nome }}
         </div>
         
         <button type="submit" class="btn btn-primary">Cadastrar compra</button>
-        
-        
-    
+
     </form>
-    
 
 
+@endsection
 @section('script')
+<script src="{{Module::asset('cliente:js/views/pedido/validations.js')}}"></script>
+<script src="{{Module::asset('cliente:js/views/pedido/inputmask.js')}}"></script>
 <script>
-   
-     $(document).on('click', '#adicionar-produto', function(){
-            $('.excluir-produto').parent().removeClass('d-none');
-            var pedido = $(".produto").last().clone();
-            
-            var inputs = pedido.find('select, input');
-            inputs.val("");
-            inputs.map((i, input)=> {
-                var match = $(input).attr('name').match(/\[(\d+)]/g)[0]
-                var contador = parseInt(match.replace('[','').replace(']',''))+1
-                var newName = $(input).attr('name').replace(match, `[${contador}]`)
-                $(input).attr('name', newName)
-            })
+    $(document).on('click', '#adicionar-produto', function(){
+        $('.excluir-produto').parent().removeClass('d-none');
+        var pedido = $(".produto").last().clone();
+        pedido.find('.error').remove();
+        var inputs = pedido.find('select, input');
+        inputs.val("");
+        inputs.map((i, input)=> {
+            var match = $(input).attr('name').match(/\[(\d+)]/g)[0]
+            var contador = parseInt(match.replace('[','').replace(']',''))+1
+            var newName = $(input).attr('name').replace(match, `[${contador}]`) 
+            $(input).attr('name', newName)
+        })
 
-            pedido.appendTo($(".produtos"))
+        pedido.appendTo($(".produtos"))
+        pedido.find("[name$='[desconto]']").inputmask("decimal", {
+            'alias': 'numeric',
+            'groupSeparator': '',
+            'autoGroup': true,
+            'digits': 2,
+            'radixPoint': ".",
+            'digitsOptional': false,
+            'allowMinus': false,
+            'prefix': '',
+            'placeholder': '',
+            'min': 0,
+            'max': 100,
+            'rightAlign': false
         });
-
-        $(document).on('click', '.excluir-produto',function(){
-            if($('.produto').length == 2){
-                $(this).closest('.produto').remove();
-                $('.excluir-produto').parent().addClass('d-none');
-            }else if($('.produto').length >= 2) {
-                $(this).closest('.produto').remove();
-            }
-        });
-
-        function checkDate() {
-            var dateString = document.getElementById('data').value;
-            var arrayData = dateString.split("-");
-            var selectedDate = new Date(arrayData[0], arrayData[2], arrayData[1]);
-            var now = new Date(); 
-            if (selectedDate > now) {  //bloquear botão de cadastrar compra  
-                alert("Data inválida");
-            }
+    });
+    $(document).on('click', '.excluir-produto',function(){
+        if($('.produto').length == 2){
+            $(this).closest('.produto').remove();
+            $('.excluir-produto').parent().addClass('d-none');
+        }else if($('.produto').length >= 2) {
+            $(this).closest('.produto').remove();
         }
+    });
 
+    function checkDate() {
+        var dateString = document.getElementById('data').value;
+        var arrayData = dateString.split("-");
+        var selectedDate = new Date(arrayData[0], arrayData[2], arrayData[1]);
+        var now = new Date(); 
+        if (selectedDate > now) {  //bloquear botão de cadastrar compra  
+            alert("Data inválida");
+        }
+    }
+
+    $(document).ready(function(){
+        $(".desconto").inputmask("decimal", {
+            'alias': 'numeric',
+            'groupSeparator': '',
+            'autoGroup': true,
+            'digits': 2,
+            'radixPoint': ".",
+            'digitsOptional': false,
+            'allowMinus': false,
+            'prefix': '',
+            'placeholder': '',
+            'min': 0,
+            'max': 100,
+            'rightAlign': false
+        });
+        $('#data').mask('00/00/0000');
+    });
 
 
 
@@ -152,5 +190,4 @@ Cadastro Nova Compra - {{ $cliente->nome }}
 // });
 
 </script>
-@endsection
 @endsection
