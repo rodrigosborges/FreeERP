ac69be266e2c6a1ee6b79bf8a71edcba7cf69785
Rafael Alves <alvesrafael17@hotmail.com>
2019-09-25 21:47:56 -0300

    arrumado bug nas validações, clonagem de validações, mascara de porcentagem,

--- Modules/Cliente/Resources/assets/js/views/pedido/validations.js
+++ Modules/Cliente/Resources/assets/js/views/pedido/validations.js
@@ -9,17 +9,17 @@ $('#form').validate({
             digits: true,
             maxlength:50
        },
-       "produtos[][produto_id]":{
+       "produtos[0][produto_id]":{
             digits: true
         },
-        "produtos[][quantidade]":{
+        "produtos[0][quantidade]":{
             digits: true,
-            maxlength: 3
         }
     },
 
     messages: {}
 })
+
 
 jQuery.extend(jQuery.validator.methods, {
     date: function (value, element) {
@@ -32,20 +32,20 @@ jQuery.extend(jQuery.validator.methods, {
 
 jQuery.validator.addMethod("dataAteHoje", function (value, element) {
     var hoje = new Date()
-    var mesAtual = '' + (hoje.getMonth() + 1)
-    var diaAtual = '' + hoje.getDate()
+    var mesAtual = hoje.getMonth() + 1
+    var diaAtual = hoje.getDate()
     var anoAtual = hoje.getFullYear()
 
     var data = value;
-    var dia = data.substr(0, 2);
-    var barra1 = data.substr(2, 1);
-    var mes = data.substr(3, 2);
-    var barra2 = data.substr(5, 1);
-    var ano = data.substr(6, 4);
+    var dia = parseInt(data.substr(0, 2));
+    var barra1 = parseInt(data.substr(2, 1));
+    var mes = parseInt(data.substr(3, 2));
+    var barra2 = parseInt(data.substr(5, 1));
+    var ano = parseInt(data.substr(6, 4));
 
-    if(anoAtual < ano)                                          return (this.optional(element) || false);
-    if(anoAtual >= ano && mesAtual < mes)                       return (this.optional(element) || false);
-    if(anoAtual >= ano && mesAtual >= mes && diaAtual < dia)    return (this.optional(element) || false);
+    if(ano > anoAtual)                                          return (this.optional(element) || false);
+    if(ano == anoAtual && mes > mesAtual)                       return (this.optional(element) || false);
+    if(ano == anoAtual && mes == mesAtual && dia > diaAtual)    return (this.optional(element) || false);
     
     return (this.optional(element) || true);
 
