860382dbf8a97eaf89afd54972d68fe9661cf968
Joao Alberto <jooalberto1@gmail.com>
2019-10-02 21:54:32 -0300

    criado os timestamps dos seeds

--- Modules/Estoque/Database/Seeders/CategoriaSeedTableSeeder.php
+++ Modules/Estoque/Database/Seeders/CategoriaSeedTableSeeder.php
@@ -12,18 +12,23 @@ class CategoriaSeedTableSeeder extends Seeder{
             DB::table('categoria')->insert([
                 [
                     'nome'          => 'Teste1',
+                    'created_at'    => '2019-07-18 02:32:44'
                 ],
                 [
                     'nome'          => 'Teste2',
+                    'created_at'    => '2019-07-16 02:32:44'
                 ],
                 [
                     'nome'          => 'Teste3',
+                    'created_at'    => '2019-07-14 02:32:44'
                 ],
                 [
                     'nome'          => 'Teste4',
+                    'created_at'    => '2019-07-12 02:32:44'
                 ],
                 [
                     'nome'          => 'Teste5',
+                    'created_at'    => '2019-07-11 02:32:44'
                 ],
             ]);
         
--- Modules/Estoque/Database/Seeders/ProdutoSeedTableSeeder.php
+++ Modules/Estoque/Database/Seeders/ProdutoSeedTableSeeder.php
@@ -15,36 +15,40 @@ class ProdutoSeedTableSeeder extends Seeder{
                     'descricao'     => 'Produto de testes 1',
                     'categoria_id'  => '1',
                     'preco'         => '10',
-                    'codigo'        => '4548546585'
-
+                    'codigo'        => '4548546585',
+                    'created_at'    => '2019-08-23 02:32:44'
                 ],
                 [
                     'nome'          => 'ProdutoTeste2',
                     'descricao'     => 'Produto de testes 2',
                     'categoria_id'  => '2',
                     'preco'         => '20',
-                    'codigo'        => '8548546585'
+                    'codigo'        => '8548546585',
+                    'created_at'    => '2019-08-22 02:32:44'
                 ],
                 [
                     'nome'          => 'ProdutoTeste3',
                     'descricao'     => 'Produto de testes 3',
                     'categoria_id'  => '3',
                     'preco'         => '30',
-                    'codigo'        => '9548546585'
+                    'codigo'        => '9548546585',
+                    'created_at'    => '2019-08-21 02:32:50'
                 ],
                 [
                     'nome'          => 'ProdutoTeste4',
                     'descricao'     => 'Produto de testes 4',
                     'categoria_id'  => '4',
                     'preco'         => '40',
-                    'codigo'        => '1548546585'
+                    'codigo'        => '1548546585',
+                    'created_at'    => '2019-08-20 02:32:50'
                 ],
                 [
                     'nome'          => 'ProdutoTeste5',
                     'descricao'     => 'Produto de testes 5',
                     'categoria_id'  => '5',
                     'preco'         => '50',
-                    'codigo'        => '6548546585'
+                    'codigo'        => '6548546585',
+                    'created_at'    => '2019-08-15 02:32:50'
                 ],
             ]);
         
