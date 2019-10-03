6d0dfa1cbb2b3a45011841559cb1ab255ea9d9d8
joaoalber <jooalberto1@gmail.com>
2019-10-03 11:22:13 -0300

    Seeders padronizados

--- Modules/Estoque/Database/Seeders/MovimentacaoEstoqueSeedTableSeeder.php
+++ Modules/Estoque/Database/Seeders/MovimentacaoEstoqueSeedTableSeeder.php
@@ -12,38 +12,38 @@ class MovimentacaoEstoqueSeedTableSeeder extends Seeder
     {
         DB::table('movimentacao_estoque')->insert([
             [
-                'estoque_id' => 1,
-                'preco_custo' => 50.02,
+                'estoque_id' => '1',
+                'preco_custo' => '50.02',
                 'observacao' => 'Esse é o teste 1',
-                'quantidade' => 3,
+                'quantidade' => '3',
                 'created_at' => '2019-06-23 02:32:44'
             ],
             [
-                'estoque_id' => 2,
-                'preco_custo' => 26.23,
+                'estoque_id' => '2',
+                'preco_custo' => '26.23',
                 'observacao' => 'Esse é o teste 2',
-                'quantidade' => 4,
+                'quantidade' => '4',
                 'created_at' => '2019-06-26 02:32:44'
             ],
             [
-                'estoque_id' => 3,
-                'preco_custo' => 12.48,
+                'estoque_id' => '3',
+                'preco_custo' => '12.48',
                 'observacao' => 'Esse é o teste 3',
-                'quantidade' => 2,
+                'quantidade' => '2',
                 'created_at' => '2019-06-25 02:32:44'
             ],
             [
-                'estoque_id' => 4,
-                'preco_custo' => 36.04,
+                'estoque_id' => '4',
+                'preco_custo' => '36.04',
                 'observacao' => 'Esse é o teste 4',
-                'quantidade' => 7,
+                'quantidade' => '7',
                 'created_at' => '2019-06-22 02:32:44'
             ],
             [
-                'estoque_id' => 5,
-                'preco_custo' => 102.03,
+                'estoque_id' => '5',
+                'preco_custo' => '102.03',
                 'observacao' => 'Esse é o teste 5',
-                'quantidade' => 10,
+                'quantidade' => '10',
                 'created_at' => '2019-06-19 02:32:44'
             ]
         ]);
--- Modules/Estoque/Database/Seeders/TipoUnidadeSeedTableSeeder.php
+++ Modules/Estoque/Database/Seeders/TipoUnidadeSeedTableSeeder.php
@@ -13,27 +13,27 @@ class TipoUnidadeSeedTableSeeder extends Seeder
         DB::table('tipo_unidade')->insert([
         [
             'nome' => 'teste1',
-            'quantidade_itens' => 10,
+            'quantidade_itens' => '10',
             'created_at'    => '2019-08-02 02:32:44'
         ],
         [
             'nome' => 'teste2',
-            'quantidade_itens' => 13,
+            'quantidade_itens' => '13',
             'created_at'    => '2019-08-09 02:32:44'
         ],
         [
             'nome' => 'teste3',
-            'quantidade_itens' => 15,
+            'quantidade_itens' => '15',
             'created_at'    => '2019-08-07 02:32:44'
         ],
         [
             'nome' => 'teste4',
-            'quantidade_itens' => 8,
+            'quantidade_itens' => '8',
             'created_at'    => '2019-08-05 02:32:44'
         ],
         [
             'nome' => 'teste5',
-            'quantidade_itens' => 20,
+            'quantidade_itens' => '20',
             'created_at'    => '2019-08-04 02:32:44'
         ],
         ]);
