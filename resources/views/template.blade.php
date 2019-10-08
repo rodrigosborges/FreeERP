7b551529e338cc3383e98c7a5ec664a08d3f67f5
Diego Magno <diegommorais12@gmail.com>
2019-10-08 18:31:37 -0300

    adicionando script jquery compativel para uso de ajax e jsbootstrap

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -126,6 +126,10 @@
             </div>
         </div>
         <!-- Bootstrap JS -->
+        <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <!-- Toggle Menu Script -->
