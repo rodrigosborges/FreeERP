b3bf33d728f7403b0e93f27ca9b4a30d35767be6
kevin Natan <kevinnatan97@gmail.com>
2019-10-08 22:06:58 -0300

    back-end atestado funcionando

--- Modules/Funcionario/Entities/Atestado.php
+++ Modules/Funcionario/Entities/Atestado.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App;
+namespace Modules\Funcionario\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Atestado extends Model
-{
-    protected $table = 'atestado';
+{   protected $table = 'atestado';
     protected $fillable = ['cid_atestado','data_inicio','data_fim','quantidade_dias','funcionario_id'];
 
     public function funcionario(){
--- Modules/Funcionario/Resources/views/funcionario/atestado.blade.php
+++ Modules/Funcionario/Resources/views/funcionario/atestado.blade.php
@@ -7,34 +7,42 @@
 @section('body')
 <div class="card col-md-12 text-left" >
   <div class="card-body">
-    <form action="">
-    <div class="form-row">
-    <div class="form-group col-md-3">
-            
-            <input type="text" class="form-control" disabled value="{{$data['funcionario']->nome}}">
-        </div>
+    <form method="post" action="{{url($data['url'])}}">
+        @if($data['method'] == 'PUT')
+            @method('PUT')
+        @endif
+        @csrf
+    <input type="hidden" value="{{$data['funcionario']->id}}" name="atestado[funcionario_id]">
+    <h5>Nome do funcionário: {{$data['funcionario']->nome}}</h5>
+    <div class="form-row pt-4">     
         <div class="form-group col-md-3">
-            <label for="">Cid Atestado</label>
-            <input type="text" class="form-control">
+            <label for="">CID</label>
+            <input type="text" class="form-control" value="{{old('atestado.cid_atestado', $data['atestado'] ? $data['Atestado']->cid_atestado : '')}}" name="atestado[cid_atestado]" >
         </div>
 
         <div class="form-group col-md-3">
-            <label for="">Data de Inicio</label>
-            <input type="date" class="form-control">
+            <label for="">Data de Início</label>
+            <input type="date" class="form-control" value="{{old('atestado.data_inicio', $data['atestado'] ? $data['atestado']->data_inicio : '')}}" name="atestado[data_inicio]">
         </div>     
 
         <div class="form-group col-md-3">
             <label for="">Data de Fim</label>
-            <input type="date" class="form-control">
+            <input type="date" class="form-control" value="{{old('atestado.data_fim', $data['atestado'] ? $data['atestado']->data_fim : '')}}" name="atestado[data_fim]">
         </div>
 
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
             <label for="">Quantidade de Dias</label>
-            <input type="text" class="form-control">
+            <input type="number" class="form-control" value="{{old('atestado.quantidade_dias', $data['atestado'] ? $data['atestado']->quantidade_dias : '')}}" name="atestado[quantidade_dias]">
         </div>
-
     </div>
-    </form>
   </div>
 </div>
+
+@section('footer')
+    <div class="d-flex justify-content-end">
+        <button type="submit"  class="btn btn-success ">Salvar</button>
+    </div>
+    </form>
+@endsection
+
 @endsection
