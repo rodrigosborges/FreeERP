77d3f1e355cca30b31402fa1319133f8fbf6d8cf
Lais Rebello Tavares <rebello.tavares@gmail.com>
2019-10-08 21:11:35 -0300

    update dos cards no dashboard

--- Modules/Cliente/Resources/views/dashboard/index.blade.php
+++ Modules/Cliente/Resources/views/dashboard/index.blade.php
@@ -1,31 +1,64 @@
 @extends('cliente::template')
 @section('content') 
 
-<div class="container">
+    <div class="container">
 
         
         <div class="row">
-            
-            <div class=" card col">
-                Quadro 1 
+            <div class="col-sm-4">
+                <div class="card h-100">
+                <div class="card-body ">
+                    <h5 class="card-title">Média de Gasto por Compra</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
             </div>
-            <div class=" card col">
-                Quadro 2
-             </div>
-            <div class="card col">
-                Quadro 3
+            <div class="col-sm-4">
+                <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Total de Clientes</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
             </div>
-
+            <div class="col-sm-4">
+                <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Total de Vendas</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
         </div>
 
-        <div class="row">
-            <div class="card col">
-                Quadro 4
+
+            <div class="row mt-3">
+
+                <div class="col-sm-6">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">Valor Total das Compras (Média)</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">Produtos Vendidos (Média)</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class=" card col">
-                Quadro 5
-            </div>
-            </div>
-      </div>
+            
+            
+      
+
+        
+         
+
+    </div>
     
 @endsection
