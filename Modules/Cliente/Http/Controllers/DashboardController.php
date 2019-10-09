bc78d4364ccf71b7834216b4044e8160792a3e8e
Adston Oliveira <suportetecnico.adston@gmail.com>
2019-10-09 19:27:16 -0300

    total vendas mes, controller e rota

--- Modules/Cliente/Http/Controllers/DashboardController.php
+++ Modules/Cliente/Http/Controllers/DashboardController.php
@@ -75,4 +75,29 @@ class DashboardController extends Controller
     {
         //
     }
+
+    public function getVendasMes($ano)
+    {
+        $pedidos = Pedido::whereYear('data', '=', $ano)->get();
+
+        $dados = [
+            1 => "JAN", 2 => "FEV", 3 => "MAR", 4 => "ABR", 5 => "MAI", 6 => "JUN", 7 => "JUL", 8 => "AGO", 9 => "SET", 10 => "OUT",
+            11 => "NOV", 12 => "DEZ"
+        ];
+
+        $vl_meses = [
+            "JAN" => 0, "FEV" => 0, "MAR" => 0, "ABR" => 0, "MAI" => 0, "JUN" => 0, "JUL" => 0, "AGO" => 0,
+            "SET" => 0, "OUT" => 0, "NOV" => 0, "DEZ" => 0
+        ];
+
+        if (count($pedidos) > 0) {
+            foreach ($pedidos as $pedido) {
+                $mes = DateTime::createFromFormat('d/m/Y', $pedido->data)->format('m');
+
+                foreach ($dados as $key => $meses)
+                    if ($mes == $key)
+                        $vl_meses[$meses] += NUMBER_FORMAT($pedido->vl_total_pedido(), 2);
+            }
+        }
+    }
 }
