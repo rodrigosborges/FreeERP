aefefe23699d3d73a630f06f16216d54a7ddef08
kevin Natan <kevinnatan97@gmail.com>
2019-11-26 21:44:14 -0200

    integração

--- Modules/Funcionario/Routes/web.php
+++ Modules/Funcionario/Routes/web.php
@@ -11,42 +11,44 @@
 |
 */
 
+
+
 Route::prefix('funcionario')->group(function() {
-    Route::get('funcionario/list/{status}', 'FuncionarioController@list');
-    Route::get('cargo/search/{valor}', 'CargoController@search');
-    Route::get('cargo/list/{status}', 'CargoController@list');
-    Route::get('funcionario/ficha/{id}', 'FuncionarioController@ficha');
-    Route::get('get-cidades/{uf}', 'FuncionarioController@getCidades');
-    Route::get('funcionario/editCargo/{id}','FuncionarioController@editCargo');
-    Route::post('funcionario/editCargo/{id}','FuncionarioController@updateCargo');
-    Route::get('funcionario/downloadComprovante/{id}','FuncionarioController@downloadComprovante');
-    Route::get('ferias/controleFerias/{id}','ControleFeriasController@controleFerias');
-    Route::get('ferias/listar/{id}','FeriasController@listar');
+    Route::get('funcionario/list/{status}', 'FuncionarioController@list')->middleware('auth');
+    Route::get('cargo/search/{valor}', 'CargoController@search')->middleware('auth');
+    Route::get('cargo/list/{status}', 'CargoController@list')->middleware('auth');
+    Route::get('funcionario/ficha/{id}', 'FuncionarioController@ficha')->middleware('auth');
+    Route::get('get-cidades/{uf}', 'FuncionarioController@getCidades')->middleware('auth');
+    Route::get('funcionario/editCargo/{id}','FuncionarioController@editCargo')->middleware('auth');
+    Route::post('funcionario/editCargo/{id}','FuncionarioController@updateCargo')->middleware('auth');
+    Route::get('funcionario/downloadComprovante/{id}','FuncionarioController@downloadComprovante')->middleware('auth');
+    Route::get('ferias/controleFerias/{id}','ControleFeriasController@controleFerias')->middleware('auth');
+    Route::get('ferias/listar/{id}','FeriasController@listar')->middleware('auth');
     
     
-    Route::get('funcionario/atestado/{id}', 'FuncionarioController@CreateAtestado');
-    Route::post('funcionario/storeAtestado', 'FuncionarioController@storeAtestado');
+    Route::get('funcionario/atestado/{id}', 'FuncionarioController@CreateAtestado')->middleware('auth');
+    Route::post('funcionario/storeAtestado', 'FuncionarioController@storeAtestado')->middleware('auth');
     
-    Route::get('funcionario/listaHistorico/{id}', 'FuncionarioController@listaHistorico');
+    Route::get('funcionario/listaHistorico/{id}', 'FuncionarioController@listaHistorico')->middleware('auth');
 
     //Rotas do demissão
-    Route::get('funcionario/demissao/{id}', 'FuncionarioController@demissao');
-    Route::post('storeDemissao', 'FuncionarioController@storeDemissao');
-    Route::get('funcionario/showDemissao/{id}', 'FuncionarioController@showDemissao');
-    Route::get('destroyDemissao/{id}', 'FuncionarioController@destroyDemissao');
+    Route::get('funcionario/demissao/{id}', 'FuncionarioController@demissao')->middleware('auth');
+    Route::post('storeDemissao', 'FuncionarioController@storeDemissao')->middleware('auth');
+    Route::get('funcionario/showDemissao/{id}', 'FuncionarioController@showDemissao')->middleware('auth');
+    Route::get('destroyDemissao/{id}', 'FuncionarioController@destroyDemissao')->middleware('auth');
     
     //Rotas do FeriasController
-    Route::get('ferias/editarFerias/{id}','FeriasController@edit');
-    Route::get('ferias/{id}/show', 'FeriasController@show');
-    Route::get('ferias/indexRelatorio', 'FeriasController@indexRelatorio');
-    Route::post('ferias/listRelatorio', 'FeriasController@listRelatorio');
+    Route::get('ferias/editarFerias/{id}','FeriasController@edit')->middleware('auth');
+    Route::get('ferias/{id}/show', 'FeriasController@show')->middleware('auth');
+    Route::get('ferias/indexRelatorio', 'FeriasController@indexRelatorio')->middleware('auth');
+    Route::post('ferias/listRelatorio', 'FeriasController@listRelatorio')->middleware('auth');
 
-    Route::get('pagamento/listar/{id}','PagamentoController@listar');
-    Route::get('pagamento/novoPagamento/{id}','PagamentoController@novoPagamento');
-    Route::get('pagamento/{id}/show', 'PagamentoController@show');
+    Route::get('pagamento/listar/{id}','PagamentoController@listar')->middleware('auth');
+    Route::get('pagamento/novoPagamento/{id}','PagamentoController@novoPagamento')->middleware('auth');
+    Route::get('pagamento/{id}/show', 'PagamentoController@show')->middleware('auth');
 
-    Route::resource('controleFerias', 'ControleFeriasController');
-    Route::resource('ferias', 'FeriasController');
+    Route::resource('controleFerias', 'ControleFeriasController')->middleware('auth');
+    Route::resource('ferias', 'FeriasController')->middleware('auth');
     //Fim das rotas
 
     Route::resource('funcionario', 'FuncionarioController');
